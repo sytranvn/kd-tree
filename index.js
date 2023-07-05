260c19b4e32d44e7ed84ec40becde1cd60ae9c14
Sy Tran Dung <sytrancsvn@gmail.com>
2023-07-05 23:30:54 +0700

    bruteforce

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import Node, { insert, search, newNode, insertRec, searchRec, equalPoint } from "./kdtree";
+import Node, { KdPoint } from "./kdtree.js";
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
@@ -6,18 +6,33 @@ const points = [];
 const MAX_POINTS = 1000;
 const RADIUS = 5;
 let selectedPoint = null;
+/**
+ * @type {Node}
+ */
+let root = null;
+/**
+ * @type {KdPoint}
+ */
+let nearestPoint = null;
+let nearestPointBf = null;
 
 function generatePoints() {
 	const width = canvas.width;
 	const height = canvas.height;
+	root = null;
+	nearestPoint = null;
 	for (let i = 0; i < MAX_POINTS; i++) {
 		points[i] = {
 			x: Math.random() * width,
 			y: Math.random() * height,
 			radius: RADIUS,
 		}
+		const p = new KdPoint(points[i].x, points[i].y);
+		if (root) root.insert(p);
+		else root = new Node(p);
 	}
 }
+
 function draw() {
 	const width = canvas.width;
 	const height = canvas.height;
@@ -29,6 +44,14 @@ function draw() {
 
 	if (selectedPoint) {
 		circle(selectedPoint, 'blue')
+	}
+
+	if (nearestPoint) {
+		circle(nearestPoint, 'yellow')
+	}
+
+	if (nearestPointBf) {
+		circle(nearestPointBf, 'red')
 	}
 }
 
@@ -53,7 +76,22 @@ function setPoint(e) {
 		y: e.clientY - rect.top,
 		radius: RADIUS,
 	}
+	const nearestNode = root.nearestNeighborSearch(new KdPoint(selectedPoint.x, selectedPoint.y));
+	nearestPoint = nearestNode ? { x: nearestNode.point.axes[0], y: nearestNode.point.axes[1], radius: RADIUS } : null;
+	nearestNeighborBfSearch();
 	draw();
+}
+
+function nearestNeighborBfSearch() {
+	nearestPointBf = null;
+	let minDis = Number.POSITIVE_INFINITY;
+	for (const point of points) {
+		const dis = (point.x - selectedPoint.x)**2 + (point.y - selectedPoint.y)**2;
+		if (dis < minDis) {
+			minDis = dis;
+			nearestPointBf = point;
+		}
+	}
 }
 
 function main() {
--- kdtree.js
+++ kdtree.js
@@ -1,34 +1,110 @@
+/**
+ * @class Point k-d point
+ */
+export class KdPoint {
+	/**
+	 * @param {...number} axes axes of point
+	 */
+	constructor(...axes) {
+		/**
+		 * @type {number[]}
+		 */
+		this.axes = axes;
+
+		/**
+		 * @type {number}
+		 */
+		this.k = axes.length;
+	}
+
+	/**
+	 * @param {KdPoint} p2
+	 * @returns {boolean}
+	 */
+	equals(p2) {
+		if (this.k !== p2.k) return false;
+		for (let i = 0; i < this.k; i++) {
+			if (this.axes[i] !== p2.axes[i])
+				return false;
+		}
+		return true;
+	}
+
+	/**
+	 * @param {KdPoint} p2
+	 * @returns {number}
+	 */
+	distance(p2) {
+		return Math.sqrt(this.axes.reduce((sum, axis, index) => {
+			return sum + (axis - p2[index])**2
+		}, 0));
+	}
+
+}
 export default class Node {
 	constructor(point, left = null, right = null) {
+		/**
+		 * @type {KdPoint}
+		 */
 		this.point = point
+		/**
+		 * @type {Node}
+		 */
 		this.left = left;
+		/**
+		 * @type {Node}
+		 */
 		this.right = right;
 	}
+
+	/**
+	 * Inserts a new node and returns root of modified tree
+	 * The parameter depth is used to decide axis of comparison
+	 * @param {KdPoint} point 
+	 * @param {number} [depth=0]
+	 * @returns {Node} root node
+	 */
+	insert(point, depth = 0) {
+		const cd = depth % point.k;
+		if (point.axes[cd] < this.point.axes[cd]) {
+			if (this.left)
+				this.left.insert(point, depth + 1);
+			else
+				this.left = new Node(point);
+		} else {
+			if (this.right)
+				this.right.insert(point, depth + 1);
+			else
+				this.right = new Node(point);
+		}
+		return this;
+	}
+
+	/**
+	 * Search for point inside a tree
+	 * @param {KdPoint} point 
+	 * @param {number} [depth=0] - depth
+	 * @returns {boolean} return true if point is inside the tree
+	 */
+	search(point, depth = 0) {
+		if (!this) return false;
+		if (this.point.equals(point)) return true;
+		const cd = depth % this.point.k;
+		if (point.axes[cd] < this.point.axes[cd]) {
+			return this.left?.search(point, depth + 1);
+		}
+		return this.right?.search(point, depth + 1);
+	}
+
+	/**
+	 * @param {KdPoint} targetPoint
+	 * @param {number} [depth=0]
+	 * @returns {Node}
+	 */
+	nearestNeighborSearch(targetPoint, distance, nearest = null, depth = 0) {
+		if (!this) return null;
+		const { point: currentPoint, left, right } = this;
+		const cd = depth % currentPoint.k;
+		// if ()
+	}
 }
-
-export function newNode(point) {
-	return new Node(point);
-}
-
-// Inserts a new node and returns root of modified tree
-// The parameter depth is used to decide axis of comparison
-export function insertRec(root, point, depth) {
-
-}
-
-export function insert(root, point) {
-
-}
-
-export function equalPoint(point1, point2) {
-	return point1.x === point2.x && point1.y === point2.y;
-}
-
-export function searchRec(root, point, depth) {
-
-}
-
-export function search(root, point) {
-
-}
-
